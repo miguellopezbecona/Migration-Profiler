bc6f98249be830c936e1e72630a58f8c7e9bbf1c
miguellopezbecona <miguelopez314@gmail.com>
2017-01-19 13:14:18 +0100

    Improve test script

--- migrCPP_mp/test.sh
+++ migrCPP_mp/test.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
 #make my_profiler_thread_migration_opt
+
 execname=my_profiler_tm
+profparams="-s2000 -l400 -p5000 -P50000000"
+#profparams="-s3500 -l750 -p1000 -P7500000"
+#profparams="-s1000 -l500 -p5000 -P1000000"
 
 gcc -fopenmp -o ABC ABC.c -lnuma
 
-# Basic test
-#g++ -o $execname [^ABC]*.c perfmon/*.c thread_migration/*.c page_migration/*.c libpfm.so.4.8.0 -lnuma && time ./$execname -m -s1000 -l500 -p5000 -P1000000 ./ABC -m1 -r5000 -s1000000 -c1111 -t8
-g++ -o $execname [^ABC]*.c perfmon/*.c thread_migration/*.c page_migration/*.c libpfm.so.4.8.0 -lnuma && ./$execname -M -s2000 -l400 -p5000 -P50000000 ./ABC -m0 -r5000 -s1000000 -c1111 -t8
-#g++ -Wno-format -o $execname [^ABC]*.c perfmon/*.c thread_migration/*.c page_migration/*.c libpfm.so.4.8.0 -lnuma #&& ./$execname -M -s2000 -l400 -p5000 -P50000000 ./ABC -m0 -r5000 -s1000000 -c1111 -t8
+# Uses numactl (and in different ways) or not depending on first parameter
+if [ $# -lt 1 ]; then
+	numacommand=
+elif [ $1 == "d" ]; then
+	numacommand="numactl --cpubind=0 --membind=0"
+elif [ $1 == "ind" ]; then
+	numacommand="numactl --cpubind=1 --membind=0"
+elif [ $1 == "int" ]; then
+	numacommand="numactl --interleave=0,1"
+else
+	numacommand=
+fi
 
-# For benchmark (thread migration)
-#g++ -o $execname [^ABC]*.c perfmon/*.c thread_migration/*.c page_migration/*.c libpfm.so.4.8.0 -lnuma && ./$execname -m -s2000 -l400 -p500 -P50000000 ~/NPB3.3.1/NPB3.3-OMP/bin/lu.C.x ; pkill -9 lu.C.x
+# Set to what you want to profile
+toprofile="./ABC -m0 -r5000 -s1000000 -c1111 -t8"
+#toprofile=~/NPB3.3.1/NPB3.3-OMP/bin/lu.C.x
+#toprofile=~/NPB3.3.1/NPB3.3-OMP/bin/bt.C.x
 
-# For benchmark (page migration)
-#g++ -o $execname [^ABC]*.c perfmon/*.c thread_migration/*.c page_migration/*.c libpfm.so.4.8.0 -lnuma && ./$execname -M -s3500 -l750 -p1000 -P7500000 ~/NPB3.3.1/NPB3.3-OMP/bin/bt.C.x ; pkill -9 bt.C.x
+# Compiles and executes
+g++ -o $execname [^ABC]*.c perfmon/*.c thread_migration/*.c page_migration/*.c libpfm.so.4.8.0 -lnuma && $numacommand ./$execname -M $profparams $toprofile
 
-###
+# Does not continue if there was an error
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+### Preprocessing of generated files
 
 # Removes last comma from each CSV
 sed -i 's/.$//' acs_*.csv max_*.csv min_*.csv avg_*.csv
@@ -25,7 +44,9 @@ sed -i 's/.$//' acs_*.csv max_*.csv min_*.csv avg_*.csv
 latest=$(ls -v acs_*.csv | tail -n 1 | sed -r "s/acs_([0-9]+).csv/\1/g")
 
 # Deletes non-final CSVs (it is not necessary)
-find . ! -name "*_$latest.csv" | grep .csv | xargs rm
+find . -maxdepth 1 ! -name "*_$latest.csv" | grep .csv | xargs rm
 
 # Renames final CSVs
-rename "s/_$latest//g" *.csv
+rename "s/_$latest//g" *.csv # This works in local
+#rename "_$latest" "" *.csv # This works in server
+
